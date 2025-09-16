b37beb1d8e8dba048b1d89d1bb7a6f20694a0338
samuel dutra <samueldutra.rp@gmail.com>
2025-09-16 20:44:02 -0300

    Cokkies

--- app/checklist/[id]/page.tsx
+++ app/checklist/[id]/page.tsx
@@ -16,7 +16,7 @@ type Checklist = {
 
 export default async function ChecklistPage({ params }: { params: { id: string } }) {
 
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
